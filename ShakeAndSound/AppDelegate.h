96f53e04c511b6ea2afc88ba30d958728ddd9799
chenbo <chenbo@chenbodemacbook-pro.local>
2016-06-29 09:20:13 +0800

    Initial Commit

---
+++ ShakeAndSound/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShakeAndSound
+//
+//  Created by chenbo on 16/6/29.
+//  Copyright © 2016年 chenbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShakeAndSound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShakeAndSound
+//
+//  Created by chenbo on 16/6/29.
+//  Copyright © 2016年 chenbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
